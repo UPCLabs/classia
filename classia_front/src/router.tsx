1f4b3e6f593a8fc166d23012d3c68e8f1fdd8884
Mariopinsel <pmlaser25@gmail.com>
2026-09-21 21:24:54 -0500

    Integracion de pagina de inico
    Signed-off-by: Mariopinsel <pmlaser25@gmail.com>

--- classia_front/src/router.tsx
+++ classia_front/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginView from './views/LoginView'
 import ProfileView from './views/ProfileView'
 import RegisterView from './views/RegisterView'
+import LandingPage from './views/LandingPage'
 
 
 export default function Router() {
@@ -12,6 +13,7 @@ export default function Router() {
               <Route path='/auth/login' element={<LoginView />} />
               <Route path='/profile' element={<ProfileView/>} />
               <Route path='/auth/register' element={<RegisterView />} />
+              <Route path='/' element={<LandingPage />} />
             </Routes>
         </BrowserRouter>
     )
